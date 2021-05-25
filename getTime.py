3715c1aff7bc6abc9892ed264224693dec9abf33
PCH <park112368@gmail.com>
2021-05-25 09:30:15 +0900

    del time(HH:MM)

--- getTime.py
+++ getTime.py
@@ -3,5 +3,5 @@ import datetime # datetime module import
 
 def getTime():        
     now = datetime.datetime.now()
-    nowTime =  now.strftime('%y-%m-%d %H:%M')
+    nowTime =  now.strftime('%y-%m-%d')
     return nowTime
